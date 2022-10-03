f560bb19da3b868000fe71607695662abc78a89c
Ravi Teja <ravi.teja@avionte.com>
2022-10-03 11:06:56 +0530

    cccc

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -31,9 +32,26 @@ export class HeaderComponent implements OnInit {
   ]
 
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // onClickHome(): any {
+  //   this.router.navigateByUrl('Home');
+  // }
+
+  onClickMenus(menu: any): any {
+    var split = menu.name.split(' ');
+    console.log(split);
+    var roterLink = split.join('')
+    console.log(roterLink);
+
+    this.router.navigateByUrl(roterLink)
+  }
+
 }
+
+
